66d3c69644f202728b04b6c207d1c172140b0b9b
Mizuki Ishikawa <ishikawa@farend.jp>
2020-12-21 16:04:16 +0900

    Support Big Sur

--- use_ansible.sh
+++ use_ansible.sh
@@ -1,6 +1,6 @@
 
 # homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install ansible
 
 # ansibleを実行
