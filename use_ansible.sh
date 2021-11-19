239e62a8f63fc2941193279af7dcaadd78b1ff55
Mizuki Ishikawa <remember929@gmail.com>
2021-11-19 11:14:26 +0900

    Update use_ansible.sh

--- use_ansible.sh
+++ use_ansible.sh
@@ -2,6 +2,7 @@
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install ansible
+ansible-galaxy collection install community.general
 
 # ansibleを実行
 # ansible-playbook --syntax-check setup.yml -i inventory
